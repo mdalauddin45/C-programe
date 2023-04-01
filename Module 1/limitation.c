afbe9a5ed27c7fa628687099b0c095da5b180398
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-01 23:15:51 +0600

    limitaion add

---
+++ Module 1/limitation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=1000000000;
+    long long int b=1000000000000;
+    float c =2000.23423423;
+    double d =22.23423423;
+    // printf("%lld",b);
+    printf("%0.15lf",d);
+    return 0;
+}
