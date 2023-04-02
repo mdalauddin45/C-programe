e7d4efa4a596a7c31070d47cac7bbcea016282f4
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-02 23:26:38 +0600

    relational operator add

---
+++ Module 2/relationalOperator.c
@@ -0,0 +1,12 @@
+// < --less then 
+// <= -- less then equal 
+// > -- grather then 
+// >= -- grather then equal 
+// == equal equal
+// != not equal 
+#include<stdio.h>
+int main()
+{
+
+    return 0;
+}
