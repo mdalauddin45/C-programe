1a7defeab24c2959ab7296fbab5e16c5b5405559
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-02 23:33:48 +0600

    conditional operator done

---
+++ Module 2/conditionalOperator.c
@@ -0,0 +1,14 @@
+// if else 
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=100){ // tk 100 er boro ba soman holy o khabo 
+        printf("Burger Khabo");
+    }
+    else{
+        printf("sharadin kichu khabo nah");
+    }
+    return 0;
+}
