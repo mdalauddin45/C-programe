043352a6b3bf6d84ff65eb378fac3ccfd16784e0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-02 23:58:21 +0600

    Quiz add

---
+++ Module 2/Quiz.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+    int main()
+    {
+        int x = 5/2;
+        printf("%d",x);
+        return 0;
+    }
+
+#include <stdio.h>
+    int main()
+    {
+        int x = 5;
+        if (x < 10) {
+            printf("hello ");
+        }
+        if (x == 5) {
+            printf("hi");
+        }
+        else {
+            printf("no");
+        }
+        return 0;
+    }
+
+#include <stdio.h>
+    int main()
+    {
+        int x=0;
+        if (x) {
+            printf("hi");
+        }
+        else {
+            printf("how are u");
+        }
+        return 0;
+    }
+
+ #include <stdio.h>
+    int main()
+    {
+        int x = 0;
+        if (x == 1) {
+            if (x >= 0) {
+                printf("true ");
+            }
+            else {
+                printf("false");
+            }
+        }
+        return 0;
+    }
