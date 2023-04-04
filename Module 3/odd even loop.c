663728df3a0d368313214bb5eebb5dfe594b6151
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-04 23:00:01 +0600

    break statement in c solved

--- Module 3/odd even loop.c
+++ Module 3/odd even loop.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int i;
-    for(i=1;i<=5;i++){
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
         if(i%2==0){
             printf("%d - even\n",i);
         }
