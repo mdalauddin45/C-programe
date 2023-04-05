e16d7c0c5a09052885283c5e3752d3f07cfea72e
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-05 23:24:55 +0600

    Hacker rank no 5 problem solved

---
+++ Module 3.5/problem 5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i,N;
+    scanf("%d",&N);
+    for(i=1;i<=N;i++){
+        if(i%5==0){
+            printf("%d Yes\n",i);
+        }
+        else{
+            printf("%d No\n",i);
+        }
+    }
+}
