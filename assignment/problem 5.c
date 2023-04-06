b7134cc1977ccf13bb5240b7a23925d27b053e41
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-06 22:37:45 +0600

    problem 5 solved

---
+++ assignment/problem 5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int N;
+    scanf("%d",&N);
+    if(1000<N){
+        printf("I will buy Panjabi\n");
+        if(1500<=N){
+        printf("I will buy shoes\nAlisa will buy new shoes\n");
+    }
+    }
+    else{
+        printf("Bed luck");
+    }
+    return 0;
+}
