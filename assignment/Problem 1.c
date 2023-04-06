024199e32603f2bb67ad6941cc62e165ac03783f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-06 22:43:56 +0600

    peoblem solved done

--- assignment/Problem 1.c
+++ assignment/Problem 1.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
 int main(){
-    printf("Hello, World! I am learning C progrmming language. ^_^ Programming is fun and challenging. /\\/\\/\\I want to give my 100%% dedication to learn!\t I will succed one day.");
+    printf("Hello, World! I am learning C progrmming language. ^_^ Programming is fun and challenging. /\\/\\/\\I want to give my 100%% dedication to learn!\tI will succed one day.");
     return 0;
 }
