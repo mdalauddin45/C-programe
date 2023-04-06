93f9ddae767f4f964a426cea124e5cb7b2bae33b
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-06 22:12:25 +0600

    problem 3 solved

---
+++ assignment/problem 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int i,N;
+    for(i=0;i<=N;i++){
+        if(i%3==0){
+            printf("YES");
+        }
+        else{
+            printf("NO");
+        }
+    }
+    return 0;
+}
