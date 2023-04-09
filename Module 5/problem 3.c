23d43f498a5457f04d218bb175ebe34970858f52
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-09 15:35:43 +0600

    problem 3 n char solved

---
+++ Module 5/problem 3.c
@@ -0,0 +1,16 @@
+//N. Char
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%c",&a);
+    if(a>='a' && a<='z'){
+       int ans=a-32;
+        printf("%c",ans);
+    }
+    else{
+        int ans=a+32;
+        printf("%c",ans);
+    }
+    return 0;
+}
