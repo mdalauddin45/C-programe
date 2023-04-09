86902863fef4e6318bb4cc847af9bce1e3a445a8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-09 16:11:59 +0600

    problem 6  test error solved

--- Module 5/problem 6.c
+++ Module 5/problem 6.c
@@ -5,10 +5,10 @@ int main()
     int a,b,c;
     scanf("%d %d %d",&a,&b,&c);
     //minimum
-    if(a<b && a<c){
+    if(a<=b && a<=c){
         printf("%d ",a);
     }
-    else if (b<a && b<c)
+    else if (b<=a && b<=c)
     {
         printf("%d ",b);
     }
@@ -16,10 +16,10 @@ int main()
          printf("%d ",c);
     }
     // maximum
-    if(a>b && a>c){
+    if(a>=b && a>=c){
         printf("%d\n",a);
     }
-    else if (b>a && b>c)
+    else if (b>=a && b>=c)
     {
         printf("%d\n",b);
     }
