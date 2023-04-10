6fcab460cfba8a1a06c12c270bc46e2f18b35093
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-11 00:15:40 +0600

    next alphabet solved

--- Module 6.5/C_Next_Alphabet.c
+++ Module 6.5/C_Next_Alphabet.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int ch;
-    scanf("%c",&ch);
-    printf("%c",ch);
+    int a;
+    scanf("%c",&a);
+    if(a='z'){
+       int ans=a-25;
+        printf("%c",ans);
+    }
+    else{
+        int ans=a+1;
+        printf("%c",ans);
+    }
     return 0;
 }
