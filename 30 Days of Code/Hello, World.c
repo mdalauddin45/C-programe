45188f75c8c0a6d3a20d7ca3d74729f7c6adb782
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-13 23:55:47 +0600

    Hello, world problem in c solved

---
+++ 30 Days of Code/Hello, World.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char ch[105]; 
+    scanf("%[^\n]",ch); 
+    printf("Hello, World.\n");
+    printf("%s", ch);
+    return 0;
+}
