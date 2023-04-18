7e477d0bf577ad63966cf890e62a532165f0b009
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-19 04:08:38 +0600

    string compare

--- Module 11/string compare.c
+++ Module 11/string compare.c
@@ -5,14 +5,14 @@ int main()
     char a[100],b[100];
     scanf("%s %s",a,b);
     int v=strcmp(a,b);
-    // printf("%d",v);
-    if(0>v){
-        printf("A Choto\n");
-    }
-    else if(0<v){
-        printf("B Choto\n");
-    }else{
-        printf("Same\n");
-    }
+    printf("%d",v);
+    // if(0>v){
+    //     printf("A Choto\n");
+    // }
+    // else if(0<v){
+    //     printf("B Choto\n");
+    // }else{
+    //     printf("Same\n");
+    // }
     return 0;
 }
