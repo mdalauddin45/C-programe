12dae1b19a425297f56fa4ba72632cd6a4b6ffc7
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-19 02:27:52 +0600

    string compare with shortcut

---
+++ Module 11/string compare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[100],b[100];
+    scanf("%s %s",a,b);
+    int v=strcmp(a,b);
+    // printf("%d",v);
+    if(0>v){
+        printf("A Choto\n");
+    }
+    else if(0<v){
+        printf("B Choto\n");
+    }else{
+        printf("Same\n");
+    }
+    return 0;
+}
