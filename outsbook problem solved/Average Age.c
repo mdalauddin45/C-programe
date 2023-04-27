5a62e122d76e017d9df21031affa96422c163aee
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-27 17:28:22 +0600

    Average Age problem solved

---
+++ outsbook problem solved/Average Age.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int y1,y2,y3,y4,y5;
+    scanf("%d %d %d %d %d",&y1,&y2,&y3,&y4,&y5);
+    int ans=(y1+y2+y3+y4+y5)/5;
+    printf("%d\n",ans);
+    return 0;
+}
