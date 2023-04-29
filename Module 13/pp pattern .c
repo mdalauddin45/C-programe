24327643c2e9e17aca76fb7423c2c7705900bd90
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-29 22:05:23 +0600

    pp pattern soved

--- Module 13/pp pattern .c
+++ Module 13/pp pattern .c
@@ -5,16 +5,15 @@ int main()
     scanf("%d",&n);
     int s=n-1;
     int k=1;
-    for(int i=1;i<=n;i++){
+    for(int i=n;i>=1;i--){
         for(int j=1;j<=s;j++){
             printf(" ");
         }
-        for(int j=1;j<=k;j++){
-            printf("* ");
-        }
-        s--;
-        k=k+2;
-        printf("\n");
+       for(int j=i;j<=2*i-1;j++){
+        printf("* ");
+       }
+       s++;
+       printf("\n");
     }
     return 0;
 }
