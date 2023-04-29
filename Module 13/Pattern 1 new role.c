3c0f776c280ff0367c5abd7d27c8d620f644be04
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-04-29 21:16:10 +0600

    pattern 1 similar

--- Module 13/Pattern 1 new role.c
+++ Module 13/Pattern 1 new role.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,k=0;
     scanf("%d",&n);
     for(int i=0;i<=n;i++){
-        for(int j=0;j< i;j++){
+        for(int j=0;j<k;j++){
              printf("* ");
         }
+        k++;
          printf("\n");
     }
     return 0;
