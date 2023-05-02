3d5b40c189051ec4e1e229894930af41a168524c
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-02 23:37:45 +0600

    math all function

---
+++ Module 14/Math_function.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main()
+{
+    // double x;
+    // scanf("%lf",&x);
+    // int ans1=ceil(x); 
+    // int ans2=floor(x);
+    // int ans3=round(x);
+    // double ans4=sqrt(x);
+    // int a,b;
+    // scanf("%d %d",&a,&b);
+    // int ans5=pow(a,b);
+    // printf("%d",ans5);
+    // printf("%d %d %d",ans1,ans2,ans3);
+    // printf("%.0lf",ans4);
+    int y;
+    scanf("%d",&y);
+    int ans6=abs(y);
+    printf("%d",ans6);
+    return 0;
+}
