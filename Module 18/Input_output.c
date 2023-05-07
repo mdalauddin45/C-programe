c6524f6c33d7fb7604ec6f84e269c7b817ea8a62
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-07 22:12:32 +0600

    exact row coloum

--- Module 18/Input_output.c
+++ Module 18/Input_output.c
@@ -5,9 +5,14 @@ int main()
     int a[5][3];
     for(int i=0;i<5;i++){
         for(int j=0;j<3;j++){
-              printf("a[%d][%d] ",i,j);
+              scanf("%d",&a[i][j]);
         }
-      printf("\n");
+    }
+    for(int i=0;i<5;i++){
+        for(int j=0;j<3;j++){
+              printf("%d ",a[i][j]);
+        }
+        printf("\n");
     }
     return 0;
 }
---
+++ Module 18/exact_row_column.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int row,col;
+    scanf("%d %d",&row,&col);
+    int a[row][col];
+    for(int i=0;i<row;i++){
+         for(int j=0;j<col;j++){
+        scanf("%d",&a[i][j]);
+    }
+    }
+    // for(int i=0;i<row;i++){
+    //      for(int j=0;j<col;j++){
+    //     printf("%d ",a[i][j]);
+    // }
+    // printf("\n");
+    // }
+
+    // // only row output
+    // int e;
+    // scanf("%d",&e);
+    // for(int i=0;i<col;i++){
+    //     printf("%d ",a[e][i]);
+    // }
+
+    // only coloum output
+    int e;
+    scanf("%d",&e);
+    for(int i=0;i<row;i++){
+        printf("%d\n",a[i][e]);
+    }
+    return 0;
+}
