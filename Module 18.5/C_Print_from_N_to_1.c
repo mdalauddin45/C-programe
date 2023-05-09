deda7e01dfab19c31c3a9053ca8fabe8c6f7e42f
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-09 23:41:54 +0600

    Print from N to 1

---
+++ Module 18.5/C_Print_from_N_to_1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void fun(int i,int n){
+    if(i==0) return;
+    printf("%d ",i);
+    fun(i-1,n);
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    fun(n,n);
+    return 0;
+}
