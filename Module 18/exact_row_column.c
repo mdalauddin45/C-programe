be6c25981d986fc6a38575a0cfe98b31cd111d7a
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-12 00:02:25 +0600

    problem 3 solved

--- Module 18/exact_row_column.c
+++ Module 18/exact_row_column.c
@@ -16,18 +16,18 @@ int main()
     // printf("\n");
     // }
 
-    // // only row output
-    // int e;
-    // scanf("%d",&e);
-    // for(int i=0;i<col;i++){
-    //     printf("%d ",a[e][i]);
-    // }
-
-    // only coloum output
+    // only row output
     int e;
     scanf("%d",&e);
-    for(int i=0;i<row;i++){
-        printf("%d\n",a[i][e]);
+    for(int i=0;i<col;i++){
+        printf("%d ",a[e][i]);
     }
+
+    // // only coloum output
+    // int e;
+    // scanf("%d",&e);
+    // for(int i=0;i<row;i++){
+    //     printf("%d\n",a[i][e]);
+    // }
     return 0;
 }
