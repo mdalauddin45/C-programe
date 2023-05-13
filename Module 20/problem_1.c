776b5fc2d4a333d66a900342ce02201219e59667
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-13 22:41:05 +0600

    problem 1 solved

--- Module 20/problem_1.c
+++ Module 20/problem_1.c
@@ -1,9 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int t,s,a,b,c;
-    scanf("%d %d %d %d %d",&t,&s,&a,&b,&c);
-    int d=s-(a+b+c);
-    printf("%d\n",d);
+    int row;
+    scanf("%d",&row);
+    int arr[row][4];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<4;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int r,s;
+    int result;
+    for(int i=0;i<row;i++){
+        s=0;
+        r=0;
+        for(int j=1;j<4;j++){
+            s=s+arr[i][j];
+        }
+        for(int j=0;j<1;j++){
+            r=r+arr[i][j];
+        }
+        result=r-s;
+        printf("%d\n",result);
+    }
     return 0;
 }
