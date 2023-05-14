525d8e12325b3da614df91d9079c67939b79b986
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-14 22:13:13 +0600

    Who win c problem solved

--- Module 20/Who Win.c
+++ Module 20/Who Win.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n;
+    int n,X1,X2;
     scanf("%d",&n);
     int tiger=0;
     int pathan=0;
@@ -13,19 +13,29 @@ int main()
         }   
     }
     for(int i=0;i<n;i++){
-        for(int j=1;j<2;j++){
-            if(arr[i]>arr[j]){
-                tiger++;
-            }
-            else if(arr[i]<arr[j]){
-                pathan++;
-            }
+       X1=0;
+       X2=0;
+        for(int j=0;j<2;j++){
+            X1=arr[i][0];
         }   
+        for(int j=0;j<2;j++){
+            X2=arr[i][1];
+        }   
+        if(X1>X2){
+            tiger++;
+        }else if (X1<X2)
+        {
+            pathan++;
+        }else{
+            draw++;
+        }
     }
-    if(tiger==pathan){
-        draw++;
+    if(pathan>tiger){
+        printf("Pathan\n");
+    }else if(tiger>pathan){
+        printf("Tiger\n");
+    }else{
+        printf("Draw\n");
     }
-      printf("Tiger - %d\nPathan - %d\nDraw - %d\n",tiger,pathan,draw);
-
     return 0;
 }
