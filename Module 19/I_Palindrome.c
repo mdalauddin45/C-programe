eb83aef6710586ce861021a6cfa180c40e271467
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-15 00:36:14 +0600

    palindrom solved

--- Module 19/I_Palindrome.c
+++ Module 19/I_Palindrome.c
@@ -1,6 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    
+    char a[100],b[100];
+    scanf("%s",&a);
+    strcpy(b,a);
+   int i=0,j=strlen(a)-1;
+   while (i<j)
+   {
+    int temp=b[i];
+    b[i]=b[j];
+    b[j]=temp;
+    i++;
+    j--;
+   }
+   if(strcmp(a,b)==0){
+    printf("YES\n");
+   }else{
+    printf("NO\n");
+   }
     return 0;
 }
