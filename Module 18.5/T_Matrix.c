341a3bea8b7204c30ef80d604d2198d3f73029c8
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-19 22:58:04 +0600

    submittion error

--- Module 18.5/T_Matrix.c
+++ Module 18.5/T_Matrix.c
@@ -1,6 +1,29 @@
 #include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
 int main()
 {
-    
+    int n;
+    scanf("%d",&n);
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int x=0,y=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                x=x+arr[i][j];
+            }
+            if(i+j==n-1){
+                y=y+arr[i][j];
+            }
+        } 
+    }
+    int sum=abs(x)+abs(y);
+        printf("%d",sum);
     return 0;
 }
