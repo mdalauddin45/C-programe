9a4837310385a6d8817d9a07d422984336ec0882
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-19 20:51:36 +0600

    triangle run in vs code but no accept hacker rank

--- Contest/First/Triangle.c
+++ Contest/First/Triangle.c
@@ -5,13 +5,12 @@ int main()
     scanf("%d %d %d",&a,&b,&c);
     if(a>=0 && a<=100 && b>=0 && b<=100 && c>=0 && c<=100){
         if(a==b && b==c && c==a && a!=0 && b!=0 && c!=0){
-              printf("YES\n");
+              printf("YES");
         }else{
-            printf("NO\n");
+            printf("NO");
         }
-      
     }else{
-        printf("NO\n");
+        printf("NO");
     }
     return 0;
 }
