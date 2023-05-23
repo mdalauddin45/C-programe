ab1516cde408b4412edd49712ce43d88d755e936
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-24 00:41:26 +0600

    d_difference solved

--- Practice in C/D_Difference.cpp
+++ Practice in C/D_Difference.cpp
@@ -1,9 +1,9 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main(){
-    int a,b,c,d;
+    long long int a,b,c,d,x;
     cin>>a>>b>>c>>d;
-    int ans=(a*b)-(c*d);
-    cout<<"Difference = "<<ans<<endl;
+     x=(a*b)-(c*d);
+    cout<<"Difference = "<<x<<endl;
     return 0;
 }
