cb6cf0c698ad6073502f24942a1d918e8d01f32d
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-05-24 00:33:32 +0600

    basic problem in c++

---
+++ Practice in C/B_Basic_Data_Types.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    long long int b;
+    char ch;
+    float c;
+    double d;
+    cin>>a>>b>>ch>>c>>d;
+    cout<<a<<endl<<b<<endl<<ch<<endl<<c<<endl<<d<<endl;
+    return 0;
+}
